5f21168a20227eaef658f3708ea2c22df0b26f15
Joey <josephrobertlopez@gmail.com>
2023-12-17 14:14:50 -0600

    Update hello.py

--- lambda/hello.py
+++ lambda/hello.py
@@ -1,6 +1,6 @@
 def lambda_handler(event, context):
     response = {
         'statusCode': 200,
-        'body': 'Hello World from dev!'
+        'body': 'Hello World from dev!!'
     }
     return response
